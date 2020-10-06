d9e3cf3f87b2ee1290125d88216ee1deceee1cc8
alirezakhoramimn <alirezakhoramimn@gmail.com>
2020-10-06 21:09:26 +0330

    Fixed the bugs

--- blog/schema.py
+++ blog/schema.py
@@ -3,17 +3,17 @@ from graphene_django import DjangoObjectType
 from .models import Category, Article,Comment
 
 class ArticleType(DjangoObjectType):
-	class Meta:
-		model = Article
-		fields = '__all__'
+    class Meta:
+        model = Article
+        fields = '__all__'
 		
 	
 class Query(graphene.ObjectType):
-	all_articles = graphene.List(ArticleType)
+    all_articles = graphene.List(ArticleType)
+    article_by_slug = grpahene.List(ArticleType, slug=grpahene.String())
 	
-	
-	def resolve_articles(root, info):
-		return Article.objects.all()
+    def resolve_articles(root, info):
+        return Article.objects.all()
 
     def resolve_category_by_name(root, info, name):
         try:
@@ -21,8 +21,8 @@ class Query(graphene.ObjectType):
         except Category.DoesNotExist:
             return None
 
-	def resolve_article_by_slug(root,info, slug):
-		return Article.objects.filter(slug=slug)
+    def resolve_article_by_slug(root,info, slug):
+        return Article.objects.filter(slug=slug)
 		
 		
 		
